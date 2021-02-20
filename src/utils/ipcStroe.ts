c0e3b9661636dfd13d3b69097f6b5f04ad92f70c
waterbeside <waterbeside@163.com>
2021-02-20 15:56:14 +0800

    feat: easy to ipc store

---
+++ src/utils/ipcStroe.ts
@@ -0,0 +1,46 @@
+let ipcRenderer: any = null
+if (typeof require === 'function') {
+  const electron = require('electron')
+  ipcRenderer = electron.ipcRenderer
+}
+
+console.log('typeof require', typeof require)
+interface IpcStoreReturn {
+  get: () => Promise<any>
+  set: (value?: any) => any
+  del: () => any
+}
+
+function ipcStore(key: string): IpcStoreReturn {
+  if (!ipcRenderer) {
+    console.log('no ipcRenderer')
+    return {
+      get: () => Promise.reject(false),
+      set: () => false,
+      del: () => false
+    }
+  }
+  return {
+    get() {
+      console.log('test get', key)
+      return new Promise((resolve, reject) => {
+        ipcRenderer
+          .invoke('store:get', key)
+          .then((res: any) => {
+            resolve(res)
+          })
+          .catch((error: any) => {
+            reject(error)
+          })
+      })
+    },
+    set(value: any) {
+      return ipcRenderer.send('store:set', { key, value })
+    },
+    del() {
+      ipcRenderer.send('store:delete', 'foo')
+    }
+  }
+}
+
+export default ipcStore
