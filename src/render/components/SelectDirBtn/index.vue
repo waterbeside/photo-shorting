658b098b2d04c90d017ca423f98d2a47bd2b3eda
waterbeside <waterbeside@163.com>
2021-03-15 17:47:42 +0800

    feat: 选择本地文件夹按钮

---
+++ src/render/components/SelectDirBtn/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="select-dir-btn" @click="selectDir">
+    <FolderOutlined v-if="isNoSlot" />
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+  import { ref, defineComponent, watch, computed } from 'vue'
+  import { FolderOutlined } from '@ant-design/icons-vue'
+
+  const ipcRenderer: any = typeof require === 'function' ? require('electron').ipcRenderer : null
+
+  export default defineComponent({
+    name: 'SelectDirBtn',
+    components: {
+      FolderOutlined
+    },
+    props: {
+      value: {
+        type: String,
+        default: ''
+      },
+      placeholder: {
+        type: String,
+        default: '请选择文件夹 '
+      }
+    },
+    emits: ['update:value', 'after-selected'],
+    setup(props, ctx) {
+      // data
+      const dirValue = ref(props.value)
+      // watch
+      watch(dirValue, (val) => {
+        ctx.emit('update:value', val)
+      })
+      // computed
+      const isNoSlot = computed(() => {
+        return ctx.slots.default ? false : true
+      })
+      // methods
+      const selectDir = () => {
+        if (!ipcRenderer) {
+          return false
+        }
+        ipcRenderer.invoke('open-directory-dialog').then((res: any) => {
+          if (!res.canceled) {
+            dirValue.value = res.filePaths[0]
+          }
+          ctx.emit('after-selected', res)
+        })
+      }
+
+      return {
+        dirValue,
+        isNoSlot,
+        selectDir
+      }
+    }
+  })
+</script>
+
+<style lang="scss" scoped>
+  .select-dir-btn {
+    display: inline-block;
+    cursor: pointer;
+    color: #888;
+  }
+</style>
